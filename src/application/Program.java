f012d6864015ed8923d178daa0924b577fffcf98
felipefinity <felipe-augusto1997@hotmail.com>
2020-06-10 19:44:59 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -36,5 +36,10 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n==== TEST 5: seller update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 }
